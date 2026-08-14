d20dc7cad1b45fd7d5ad4124caa7e1ce6d2ee9df
paulo870 <paulo@poliglotaangola.com>
2026-08-14 10:32:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ audioBtn.addEventListener("click", function(e) {
     // Define audio tracks for all units (Student Book)
     const studentBookAudioTracks = {
         1: {4:["page4_Track_02","page4_Track_03"],5:["page5_Track_04"],6:["page6_Track_05","page6_Track_06"],8:["page8_Track_07"],9:["page9_Track_08","page9_Track_09"]},
-        2: {13:["page13_Track_2.1"],14:["page14_Track_2.2"],15:["page15_Track_2.3","page15_Track_2.4"]},
+        2: {10:["page10_Track_10","page10_Tracl_11"],11:["page11_Track_12"],12:["page12_Track_13","page12_Track_14"],13:["page13_Track_15","page13_Track_16"],14:["page14_Track_17"],15:["page15_Track_18","page15_Track_19"],16:["page16_Track_20"],17:["page17_Track_21"]},
         3: {20:["page20_Track_3.1","page20_Track_3.2"],21:["page21_Track_3.3"]},
         4: {26:["page26_Track_4.1"],28:["page28_Track_4.2"],29:["page29_Track_4.3","page29_Track_4.4"],30:["page30_Track_4.5"]},
         5: {33:["page33_Track_5.1"],34:["page34_Track_5.2","page34_Track_5.3"],35:["page35_Track_5.4"]},
